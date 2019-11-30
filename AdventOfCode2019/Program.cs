7f6e32087390b7004d6e6c92cbf7cbc1c2b4696e
Kim Kræmer <kim.kraemer@gmail.com>
2019-11-30 11:12:25 +0100

    Template

--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace AdventOfCode2019
 {
     class Program
     {
+        static IEnumerable<AbstractSolver> Solvers;
+
         static void Main(string[] args)
         {
+            Solvers = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.Namespace == @"AdventOfCode2019.Solvers")
+                .Select(s => (AbstractSolver)Activator.CreateInstance(s))
+                .OrderBy(s => s.Day);
+
+            var solver = Solvers.Last();
+            Console.WriteLine("Solving Part 1");
+            Console.WriteLine("Solution to Part 1: {0}", solver.Part1());
+
+            Console.WriteLine("\r\nSolving Part 2");
+            Console.WriteLine("Solution to Part 2: {0}", solver.Part2());
+
+            Console.ReadLine();
         }
     }
 }
---
+++ AdventOfCode2019/Solvers/Day1Solver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2019.Solvers
+{
+    class Day1Solver : AbstractSolver
+    {
+        public override string Part1()
+        {
+            return "Not implemented!";
+        }
+
+        public override string Part2()
+        {
+            return "Not implemented!";
+        }
+    }
+}
