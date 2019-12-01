fff2850637fe00ae8f5594d1ce9c13800be86273
Kim Kræmer <kim.kraemer@gmail.com>
2019-12-01 06:17:23 +0100

    Day 1

--- AdventOfCode2019/Solvers/Day1Solver.cs
+++ AdventOfCode2019/Solvers/Day1Solver.cs
@@ -7,12 +7,41 @@ namespace AdventOfCode2019.Solvers
     {
         public override string Part1()
         {
-            return "Not implemented!";
+            long totalFuelRequired = 0;
+            using(var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+                    int mass = Int32.Parse(input.ReadLine());
+                    int fuelRequired = (int)Math.Floor(mass / 3.0) - 2;
+                    totalFuelRequired += fuelRequired;
+                }
+            }
+            
+            return totalFuelRequired.ToString();
         }
 
         public override string Part2()
         {
-            return "Not implemented!";
+            long totalFuelRequired = 0;
+            using (var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+                    int mass = Int32.Parse(input.ReadLine());
+                    int fuelRequired = getFuelRequired(mass);
+                    totalFuelRequired += fuelRequired;
+                }
+            }
+
+            return totalFuelRequired.ToString();
+        }
+
+        int getFuelRequired(int mass)
+        {
+            int fuel = Math.Max((int)Math.Floor(mass / 3.0) - 2, 0);
+            if (fuel > 0) fuel += (getFuelRequired(fuel));
+            return fuel;
         }
     }
 }
