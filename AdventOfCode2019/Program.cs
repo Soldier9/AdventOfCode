eb47bdad4af359a6d4d2f820a7b521ee98f63371
Kim Kræmer <kim.kraemer@gmail.com>
2019-12-02 07:04:43 +0100

    Day 2

--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -14,16 +14,31 @@ namespace AdventOfCode2019
             Solvers = Assembly
                 .GetExecutingAssembly()
                 .GetTypes()
-                .Where(t => t.Namespace == @"AdventOfCode2019.Solvers")
+                .Where(t => t.Namespace == @"AdventOfCode2019.Solvers" && t.BaseType.Name == @"AbstractSolver")
                 .Select(s => (AbstractSolver)Activator.CreateInstance(s))
                 .OrderBy(s => s.Day);
 
             var solver = Solvers.Last();
-            Console.WriteLine("Solving Part 1");
-            Console.WriteLine("Solution to Part 1: {0}", solver.Part1());
+            Console.WriteLine("Advent of Code Day {0}", solver.Day);
+            Console.WriteLine("\r\nSolving Part 1");
+            try
+            {
+                Console.WriteLine("Solution to Part 1: {0}", solver.Part1());
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine("Solution not implemented");
+            }
 
             Console.WriteLine("\r\nSolving Part 2");
-            Console.WriteLine("Solution to Part 2: {0}", solver.Part2());
+            try
+            {
+                Console.WriteLine("Solution to Part 2: {0}", solver.Part2());
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine("Solution not implemented");
+            }
 
             Console.ReadLine();
         }
---
+++ AdventOfCode2019/Solvers/Day2Solver.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019.Solvers
+{
+    class Day2Solver : AbstractSolver
+    {
+        class IntcodeCPU
+        {
+            int[] Program;
+            int IP = 0;
+
+            public int RunProgram(int[] program)
+            {
+                Program = program;
+
+                bool terminate = false;
+                while(!terminate)
+                {
+                    var opCode = Program[IP];
+                    
+                    switch (opCode)
+                    {
+                        case 1:
+                            Program[Program[IP + 3]] = Program[Program[IP + 1]] + Program[Program[IP + 2]];
+                            break;
+                        case 2:
+                            Program[Program[IP + 3]] = Program[Program[IP + 1]] * Program[Program[IP + 2]];
+                            break;
+                        case 99:
+                            terminate = true;
+                            break;
+                    }
+                    IP += 4;
+                }
+
+                return Program[0];
+            }
+
+        }
+
+
+
+        public override string Part1()
+        {
+            IntcodeCPU cpu = new IntcodeCPU();
+            int[] program;
+            
+            using(var input = File.OpenText(InputFile))
+            {
+                program = input.ReadLine().Split(',').Select(n => int.Parse(n)).ToArray();
+            }
+
+            program[1] = 1;
+            program[2] = 2;
+            return cpu.RunProgram(program).ToString();
+        }
+
+        public override string Part2()
+        {            
+            int[] program;
+            using (var input = File.OpenText(InputFile))
+            {
+                program = input.ReadLine().Split(',').Select(n => int.Parse(n)).ToArray();
+            }
+
+            bool terminate = false;
+            int result = 0;
+            for (int n = 0; n < 100; n++)
+            {
+                for (int v = 0; v < 100; v++)
+                {
+                    int[] testProgram = new int[program.Length];
+                    program.CopyTo(testProgram, 0);
+
+                    testProgram[1] = n;
+                    testProgram[2] = v;
+
+                    IntcodeCPU cpu = new IntcodeCPU();
+                    terminate = (cpu.RunProgram(testProgram) == 19690720);
+                    if (terminate)
+                    {
+                        result = n * 100 + v;
+                        break;
+                    }
+                }
+                if (terminate) break;
+            }
+
+            return result.ToString();
+
+        }
+    }
+}
