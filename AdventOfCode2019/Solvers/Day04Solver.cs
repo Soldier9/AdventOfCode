3489f2b96422cd3e60746cd1723241f7401198f4
Kim Kræmer <kim.kraemer@gmail.com>
2019-12-07 12:38:54 +0100

    Cleanup

--- AdventOfCode2019/Solvers/Day04Solver.cs
+++ AdventOfCode2019/Solvers/Day04Solver.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2019.Solvers
+﻿namespace AdventOfCode2019.Solvers
 {
     class Day04Solver : AbstractSolver
     {
