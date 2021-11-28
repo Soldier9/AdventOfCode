a22b3a7c331ccde01d74dfcfa8b3e0f05fc797b8
Kim Kræmer <kim.kraemer@gmail.com>
2021-11-28 11:01:27 +0100

    Preparation for 2021

---
+++ AdventOfCode/Solvers/2021/Day1Solver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Solvers.Year2021
+{
+    class Day1Solver : AbstractSolver
+    {
+        private const int target = 2021;
+        
+        public override string Part1()
+        {
+            using(var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+
+                }
+            }
+            
+            return "No solution found";
+        }
+
+        public override string Part2()
+        {
+            using (var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+
+                }
+            }
+
+            return "No solution found";
+        }
+    }
+}
