b8961a150dc694e981c2960587d01a9528b7b044
Kim Kræmer <kim.kraemer@gmail.com>
2021-12-01 06:15:51 +0100

    Day1

--- AdventOfCode/Solvers/2021/Day1Solver.cs
+++ AdventOfCode/Solvers/2021/Day1Solver.cs
@@ -9,28 +9,48 @@ namespace AdventOfCode.Solvers.Year2021
         
         public override string Part1()
         {
+            int result = 0;
             using(var input = File.OpenText(InputFile))
             {
+                int oldMeasure = int.MaxValue;
                 while (!input.EndOfStream)
                 {
-
+                    int newMeasure = int.Parse(input.ReadLine());
+                    if (newMeasure > oldMeasure) result++;
+                    oldMeasure = newMeasure;
                 }
             }
             
-            return "No solution found";
+            return result.ToString();
         }
 
         public override string Part2()
         {
+            List<int> measures = new List<int>();
             using (var input = File.OpenText(InputFile))
             {
+                
                 while (!input.EndOfStream)
                 {
-
+                    measures.Add(int.Parse(input.ReadLine()));
                 }
             }
 
-            return "No solution found";
+            int result = 0;
+            int measure = 0;
+            int prevMeasure = int.MaxValue;
+            for (int i = 0; i < measures.Count; i++)
+            {
+                measure += measures[i];
+                if (i > 1)
+                {
+                    if (i > 2) measure -= measures[i - 3];
+                    if (measure > prevMeasure) result++;
+                    prevMeasure = measure;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
