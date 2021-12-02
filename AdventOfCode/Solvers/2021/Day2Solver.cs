802e4bc57d8f9f5e2f0c37067f54930088f3c4a7
Kim Kræmer <kim.kraemer@gmail.com>
2021-12-02 06:12:00 +0100

    Day 2

---
+++ AdventOfCode/Solvers/2021/Day2Solver.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+namespace AdventOfCode.Solvers.Year2021
+{
+    class Day2Solver : AbstractSolver
+    {
+        private const int target = 2021;
+
+        public override string Part1()
+        {
+            int horizontal = 0;
+            int deepth = 0;
+
+            using (var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+                    string[] instr = input.ReadLine().Split(new char[] { ' ' });
+                    switch (instr[0])
+                    {
+                        case "forward": horizontal += int.Parse(instr[1]); break;
+                        case "down": deepth += int.Parse(instr[1]); break;
+                        case "up": deepth -= int.Parse(instr[1]); break;
+                    }
+                }
+            }
+
+            return (horizontal * deepth).ToString();
+        }
+
+        public override string Part2()
+        {
+            int horizontal = 0;
+            int aim = 0;
+            int deepth = 0;
+
+            using (var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+                    string[] instr = input.ReadLine().Split(new char[] { ' ' });
+                    switch (instr[0])
+                    {
+                        case "forward":
+                            horizontal += int.Parse(instr[1]);
+                            deepth += (int.Parse(instr[1]) * aim);
+                            break;
+                        case "down": aim += int.Parse(instr[1]); break;
+                        case "up": aim -= int.Parse(instr[1]); break;
+
+                    }
+                }
+            }
+
+            return (horizontal * deepth).ToString();
+        }
+    }
+}
