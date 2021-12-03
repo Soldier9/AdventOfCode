598ddb937e46ed096f32b0037c5d867a28388c79
Kim Kræmer <kim.kraemer@gmail.com>
2021-12-03 08:26:52 +0100

    Day 3

---
+++ AdventOfCode/Solvers/2021/Day3Solver.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Solvers.Year2021
+{
+    class Day3Solver : AbstractSolver
+    {
+        public override string Part1()
+        {
+            List<int> bits = null;
+            int inputs = 0;
+
+            using (var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+                    string line = input.ReadLine();
+                    if (bits == null) bits = new List<int>(new int[line.Length]);
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] == '1') bits[i]++;
+                    }
+                    inputs++;
+                }
+            }
+
+            int gamma = 0;
+            int epsilon = 0;
+            int half = inputs / 2;
+            for (int i = 0; i < bits.Count; i++)
+            {
+                if (bits[i] > half) gamma ^= (1 << (bits.Count - 1 - i));
+                else epsilon ^= (1 << (bits.Count - 1 - i));
+            }
+            return (gamma * epsilon).ToString();
+        }
+
+        public override string Part2()
+        {
+            List<string> orgInputs = new List<string>();
+            using (var input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream) orgInputs.Add(input.ReadLine());
+            }
+
+            int oxygen = 0;
+            List<string> inputs = orgInputs.ToList<string>();
+            for (int i = 0; i < inputs[0].Length; i++)
+            {
+                if (inputs.Count == 1)
+                {
+                    oxygen = Convert.ToInt32(inputs[0], 2);
+                    break;
+                }
+
+                int ones = inputs.Count(input => input[i] == '1');
+                if (ones >= inputs.Count / 2) inputs.RemoveAll(s => s[i] == '0');
+                else inputs.RemoveAll(s => s[i] == '1');
+            }
+
+
+            int co2 = 0;
+            inputs = orgInputs.ToList<string>();
+            for (int i = 0; i < inputs[0].Length; i++)
+            {
+                if (inputs.Count == 1)
+                {
+                    co2 = Convert.ToInt32(inputs[0], 2);
+                    break;
+                }
+
+                int ones = inputs.Count(input => input[i] == '1');
+                if (ones < inputs.Count / 2) inputs.RemoveAll(s => s[i] == '0');
+                else inputs.RemoveAll(s => s[i] == '1');
+            }
+
+            return (oxygen * co2).ToString();
+        }
+    }
+}
