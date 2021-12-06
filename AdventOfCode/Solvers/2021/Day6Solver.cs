08d1052b7286f813a510a2bd0a0f733c0412e15b
Kim Kræmer <kim.kraemer@gmail.com>
2021-12-06 13:13:06 +0100

    Day6

---
+++ AdventOfCode/Solvers/2021/Day6Solver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Solvers.Year2021
+{
+    class Day6Solver : AbstractSolver
+    {
+        public override string Part1()
+        {
+            List<int> fish = new List<int>();
+
+            using (var input = File.OpenText(InputFile))
+            {
+                fish = new List<int>(input.ReadLine().Split(',').Select(f => int.Parse(f)));
+            }
+
+            for (int i = 0; i < 80; i++)
+            {
+                int count = fish.Count;
+                for (int n = 0; n < count; n++)
+                {
+                    fish[n]--;
+                    if (fish[n] < 0)
+                    {
+                        fish[n] = 6;
+                        fish.Add(8);
+                    }
+                }
+            }
+
+            return fish.Count().ToString();
+        }
+
+        public override string Part2()
+        {
+            List<Int64[]> fish = new List<Int64[]>();
+
+            using (var input = File.OpenText(InputFile))
+            {
+                var tmpFish = new List<int>(input.ReadLine().Split(',').Select(f => int.Parse(f)));
+                for (int n = 0; n < 9; n++)
+                {
+                    fish.Add(new Int64[2] { n, 0 });
+                }
+
+                foreach (var f in tmpFish)
+                {
+                    fish[f][1]++;
+                }
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                int group0Index = 0;
+                int group6Index = 0;
+
+                for (int groupIdx = 0; groupIdx < fish.Count; groupIdx++)
+                {
+                    if (fish[groupIdx][0] == 0) group0Index = groupIdx;
+                    fish[groupIdx][0]--;
+                    if (fish[groupIdx][0] == 6) group6Index = groupIdx;
+                }
+
+                fish[group6Index][1] += fish[group0Index][1];
+                fish[group0Index][0] = 8;
+            }
+
+            Int64 result = 0;
+            foreach (var group in fish) result += group[1];
+
+            return result.ToString();
+        }
+    }
+}
