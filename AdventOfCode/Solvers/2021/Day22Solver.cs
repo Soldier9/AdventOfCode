e27e7f181d12d0e9e6a573f629c6ac3665893182
Kim Kræmer <kim.kraemer@gmail.com>
2021-12-22 14:06:18 +0100

    Day 22 (part 2 sloooooow)

---
+++ AdventOfCode/Solvers/2021/Day22Solver.cs
@@ -0,0 +1,151 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solvers.Year2021
+{
+    class Day22Solver : AbstractSolver
+    {
+        public override string Part1()
+        {
+            Dictionary<(int x, int y, int z), bool> cubes = new();
+            Regex parser = new(@"(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)");
+
+            using (StreamReader input = File.OpenText(InputFile))
+            {
+                while (!input.EndOfStream)
+                {
+                    Match line = parser.Match(input.ReadLine()!);
+                    (int min, int max) xRange = (int.Parse(line.Groups[2].Value), int.Parse(line.Groups[3].Value));
+                    (int min, int max) yRange = (int.Parse(line.Groups[4].Value), int.Parse(line.Groups[5].Value));
+                    (int min, int max) zRange = (int.Parse(line.Groups[6].Value), int.Parse(line.Groups[7].Value));
+
+                    bool state = line.Groups[1].Value == "on";
+
+                    for (int x = Math.Max(-50, xRange.min); x <= Math.Min(xRange.max, 50); x++)
+                    {
+                        for (int y = Math.Max(-50, yRange.min); y <= Math.Min(yRange.max, 50); y++)
+                        {
+                            for (int z = Math.Max(-50, zRange.min); z <= Math.Min(zRange.max, 50); z++)
+                            {
+                                if (!cubes.ContainsKey((x, y, z))) cubes.Add((x, y, z), state);
+                                else cubes[(x, y, z)] = state;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int result = 0;
+            for (int x = -50; x <= 50; x++)
+            {
+                for (int y = -50; y <= 50; y++)
+                {
+                    for (int z = -50; z <= 50; z++)
+                    {
+                        if (cubes.ContainsKey((x, y, z)) && cubes[(x, y, z)]) result++;
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public override string Part2()
+        {
+            Dictionary<((int x, int y, int z) min, (int x, int y, int z) max), long> cubes = new();
+            Regex parser = new(@"(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)");
+
+            using (StreamReader input = File.OpenText(InputFile))
+            {
+                int linesProcessed = 0;
+                while (!input.EndOfStream)
+                {
+                    Match line = parser.Match(input.ReadLine()!);
+                    (int min, int max) xRange = (int.Parse(line.Groups[2].Value), int.Parse(line.Groups[3].Value));
+                    (int min, int max) yRange = (int.Parse(line.Groups[4].Value), int.Parse(line.Groups[5].Value));
+                    (int min, int max) zRange = (int.Parse(line.Groups[6].Value), int.Parse(line.Groups[7].Value));
+
+                    bool state = line.Groups[1].Value == "on";
+
+                    ((int x, int y, int z) min, (int x, int y, int z) max) newCube;
+                    newCube.min.x = xRange.min;
+                    newCube.min.y = yRange.min;
+                    newCube.min.z = zRange.min;
+                    newCube.max.x = xRange.max + 1;
+                    newCube.max.y = yRange.max + 1;
+                    newCube.max.z = zRange.max + 1;
+
+                    Dictionary<((int x, int y, int z) min, (int x, int y, int z) max), long> newCubes = new();
+                    foreach (KeyValuePair<((int x, int y, int z) min, (int x, int y, int z) max), long> cube in cubes)
+                    {
+                        foreach (((int x, int y, int z) min, (int x, int y, int z) max) splitCube in SplitCube(cube.Key, newCube))
+                        {
+                            if (!Contains(newCube, splitCube)) _ = newCubes.TryAdd(splitCube, Volume(splitCube));
+                        }
+                    }
+                    if (state) _ = newCubes.TryAdd(newCube, Volume(newCube));
+
+                    cubes = newCubes;
+                    Program.PrintData("(" + ++linesProcessed + " / 420) " + cubes.Count + " cubes", 0, true);
+                }
+            }
+
+            long result = cubes.Sum(c => c.Value);
+            return result.ToString();
+        }
+
+        private static long Volume(((int x, int y, int z) min, (int x, int y, int z) max) cube)
+        {
+            long x = Math.Abs(cube.max.x - cube.min.x);
+            long y = Math.Abs(cube.max.y - cube.min.y);
+            long z = Math.Abs(cube.max.z - cube.min.z);
+
+            return x * y * z;
+        }
+
+        private static bool Contains(((int x, int y, int z) min, (int x, int y, int z) max) cube1, ((int x, int y, int z) min, (int x, int y, int z) max) cube2)
+        {
+            return (cube1.min.x <= cube2.min.x && cube1.min.y <= cube2.min.y && cube1.min.z <= cube2.min.z &&
+                cube1.max.x >= cube2.max.x && cube1.max.y >= cube2.max.y && cube1.max.z >= cube2.max.z);
+        }
+
+        private static List<((int x, int y, int z) min, (int x, int y, int z) max)> SplitCube(((int x, int y, int z) min, (int x, int y, int z) max) cube1, ((int x, int y, int z) min, (int x, int y, int z) max) cube2)
+        {
+            List<((int x, int y, int z) min, (int x, int y, int z) max)> result = new();
+
+            List<int> xSplits = new();
+            List<int> ySplits = new();
+            List<int> zSplits = new();
+
+            xSplits.Add(cube1.min.x);
+            if (cube1.min.x <= cube2.min.x && cube2.min.x < cube1.max.x) xSplits.Add(cube2.min.x);
+            if (cube1.min.x < cube2.max.x && cube2.max.x <= cube1.max.x) xSplits.Add(cube2.max.x);
+            xSplits.Add(cube1.max.x);
+
+            ySplits.Add(cube1.min.y);
+            if (cube1.min.y <= cube2.min.y && cube2.min.y < cube1.max.y) ySplits.Add(cube2.min.y);
+            if (cube1.min.y < cube2.max.y && cube2.max.y <= cube1.max.y) ySplits.Add(cube2.max.y);
+            ySplits.Add(cube1.max.y);
+
+            zSplits.Add(cube1.min.z);
+            if (cube1.min.z <= cube2.min.z && cube2.min.z < cube1.max.z) zSplits.Add(cube2.min.z);
+            if (cube1.min.z < cube2.max.z && cube2.max.z <= cube1.max.z) zSplits.Add(cube2.max.z);
+            zSplits.Add(cube1.max.z);
+
+            for (int ix = 0; ix < xSplits.Count - 1; ix++)
+            {
+                (int min, int max) xRange = (xSplits[ix], xSplits[ix + 1]);
+                for (int iy = 0; iy < ySplits.Count - 1; iy++)
+                {
+                    (int min, int max) yRange = (ySplits[iy], ySplits[iy + 1]);
+                    for (int iz = 0; iz < zSplits.Count - 1; iz++)
+                    {
+                        (int min, int max) zRange = (zSplits[iz], zSplits[iz + 1]);
+                        ((int x, int y, int z) min, (int x, int y, int z) max) newCube = ((xRange.min, yRange.min, zRange.min), (xRange.max, yRange.max, zRange.max));
+                        result.Add(newCube);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
