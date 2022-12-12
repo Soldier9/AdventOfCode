07c904e49f37f594a5cde62fda4b3792f91ebad2
Kim Kræmer <kim.kraemer@gmail.com>
2022-12-12 12:36:17 +0100

    2022-12 Path added to visualization

--- AdventOfCode/Solvers/2022/Day12Solver.cs
+++ AdventOfCode/Solvers/2022/Day12Solver.cs
@@ -56,15 +56,19 @@ namespace AdventOfCode.Solvers.Year2022
 
             return costs[dest].ToString();
         }
-        private bool VisitPos((int x, int y) pos, HashSet<(int x, int y)> dests, Dictionary<(int x, int y), int> costs, PriorityQueue<(int x, int y), int> queue, bool part2 = false)
+
+        private List<(int x, int y)> getNeighbors((int x, int y) pos)
         {
             List<(int x, int y)> neighbors = new();
             if (pos.y > 0) neighbors.Add((pos.x, pos.y - 1));
             if (pos.y < height - 1) neighbors.Add((pos.x, pos.y + 1));
             if (pos.x > 0) neighbors.Add((pos.x - 1, pos.y));
             if (pos.x < width - 1) neighbors.Add((pos.x + 1, pos.y));
-
-            foreach ((int x, int y) neighbor in neighbors)
+            return neighbors;
+        }
+        private bool VisitPos((int x, int y) pos, HashSet<(int x, int y)> dests, Dictionary<(int x, int y), int> costs, PriorityQueue<(int x, int y), int> queue, bool part2 = false)
+        {
+            foreach ((int x, int y) neighbor in getNeighbors(pos))
             {
                 if ((!part2 && map[pos] >= map[neighbor] - 1 && costs[neighbor] > costs[pos] + 1)
                     || (part2 && map[pos] <= map[neighbor] + 1 && costs[neighbor] > costs[pos] + 1))
@@ -75,7 +79,29 @@ namespace AdventOfCode.Solvers.Year2022
                 }
             }
 
-            if (Program.VisualizationEnabled) Print(costs);
+            if (Program.VisualizationEnabled)
+            {
+                List<(int x, int y)> path = new();
+                (int x, int y) backTrace;
+                int cost;
+                queue.TryPeek(out backTrace, out cost);
+
+                path.Add(backTrace);
+                while(cost > 0)
+                {
+                    foreach((int x, int y) neighbor in getNeighbors(backTrace)) {
+                        if (costs[neighbor] == cost - 1)
+                        {
+                            backTrace = neighbor;
+                            cost--;
+                            path.Add(backTrace);
+                            break;
+                        }
+                    }
+                }
+                path.Reverse();
+                Print(costs, path);
+            }
             while (queue.Count > 0)
             {
                 if (VisitPos(queue.Dequeue(), dests, costs, queue, part2)) return true;
@@ -83,7 +109,7 @@ namespace AdventOfCode.Solvers.Year2022
             return false;
         }
 
-        private void Print(Dictionary<(int x, int y), int> costs)
+        private void Print(Dictionary<(int x, int y), int> costs, List<(int x, int y)> path)
         {
             StringBuilder sb = new();
 
@@ -91,9 +117,15 @@ namespace AdventOfCode.Solvers.Year2022
             {
                 for (int x = 0; x < width; x++)
                 {
-
+                    bool hasColor = false;
+                    if (path.Contains((x, y)))
+                    {
+                        sb.Append("\u001b[31m");
+                        hasColor = true;
+                    }
                     if (costs[(x, y)] < int.MaxValue) _ = sb.Append(costs[(x, y)].ToString()[^1]);
-                    else _ = sb.Append(map[(x, y)]);
+                    else _ = sb.Append(map[(x, y)]);                    
+                    if(hasColor) sb.Append("\u001b[0m");
                 }
                 _ = sb.Append("\r\n");
             }
