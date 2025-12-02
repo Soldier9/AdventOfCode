c29aee5a2db9743115100d607c5b61b299d6d3eb
soldier9 <kim.kraemer@gmail.com>
2025-12-02 09:00:09 +0100

    2025-2

---
+++ AdventOfCode/Solvers/2025/Day2Solver.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solvers._2025;
+
+internal class Day2Solver : AbstractSolver
+{
+    private readonly List<(long min, long max)> _ranges = [];
+
+    public override string Part1()
+    {
+        using (var input = File.OpenText(InputFile))
+        {
+            foreach (var range in input.ReadLine()!.Split(','))
+            {
+                var splitRange = range.Split('-');
+                _ranges.Add((long.Parse(splitRange[0]), long.Parse(splitRange[1])));
+            }
+        }
+
+        var testId = new Regex(@"^(\d+)\1$");
+        var result = _ranges.Sum(range => FindInvalidIDs(range, testId).Sum());
+        return result.ToString();
+    }
+
+    private List<long> FindInvalidIDs((long min, long max) range, Regex testRegex)
+    {
+        List<long> invalidIds = new();
+        for (var i = range.min; i <= range.max; i++)
+        {
+            var stringId = i.ToString();
+            if (testRegex.IsMatch(stringId)) invalidIds.Add(i);
+        }
+
+        return invalidIds;
+    }
+
+    public override string Part2()
+    {
+        var testId = new Regex(@"^(\d+)\1+$");
+        var result = _ranges.Sum(range => FindInvalidIDs(range, testId).Sum());
+        return result.ToString();
+    }
+}
